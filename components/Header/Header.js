d35e43ffc373eddee25c3510a19cb56e8c53487b
romulo bordezani <romulobordezani@gmail.com>
2019-07-26 02:26:43 -0300

    Adding support to css modules with SASS on Next.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,8 +1,8 @@
-import './Header.scss';
+import style from './Header.scss';
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={ style.header }>
       <img src="/static/images/logo-pt__large_plus.png" alt="Mercado Livre"/>
     </header>
   )
