ee96cd740d15414cb3794ba4bf3b5ff86446c8ed
romulo bordezani <romulobordezani@gmail.com>
2019-07-26 03:30:56 -0300

    Adding Favicon support trough Express, serving static files on root.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const next = require('next');
 const bodyParser = require('body-parser');
-const { PORT, dev } = require('../config');
+const serveStatic = require('serve-static');
+const path = require('path');
 
+const { PORT, dev } = require('../config');
 const NextApp = next({ dev });
 const handle = NextApp.getRequestHandler();
 
@@ -12,6 +14,7 @@ NextApp.prepare().then(() => {
   server.use(bodyParser.json());
   server.use(bodyParser.urlencoded({ extended: true }));
   server.use('/api/items', require('./routes/items.routes'));
+  server.use(serveStatic(path.resolve('./static')));
 
   // Handles all next + React Stuff not matched by Express routes
   server.get('*', (req, res) => handle(req, res));
@@ -21,5 +24,6 @@ NextApp.prepare().then(() => {
       throw err;
     }
     console.log(`Express app available on http://localhost:${PORT}`)
-  })
+  });
+
 });
