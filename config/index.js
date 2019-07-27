dac656cf7381ebb380053a3b9b679e6dc6464837
romulo bordezani <romulobordezani@gmail.com>
2019-07-27 02:18:40 -0300

    Fixing API_URL issue loading env vars

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const CONFIG = () => ({
   MELI_API: 'https://api.mercadolibre.com',
   MELI_SITE: process.env.MELI_SITE || 'MLB',
-  API_URL: process.env.API_URL || 'http://localhost:3000',
+  API_URL: process.env.API_URL,
   PORT: parseInt(process.env.PORT, 10) || 3000,
   dev: process.env.NODE_ENV !== 'production'
 });
