eb1558b294ba2d7aeb8e0e20c9e39824be52bbb1
romulo bordezani <romulobordezani@gmail.com>
2019-07-27 19:28:35 -0300

    getDecimals Util.

--- server/helpers/getDecimals.js
+++ server/helpers/getDecimals.js
@@ -1,9 +1,17 @@
 function getDecimals(n) {
+  if (typeof n === 'string') {
+    n = n.replace(',', '.'); // Replaces commas with dots
+  }
   n = Math.abs(n); // makes it positive if negative
-  const onlyDecimals = (n - Math.floor(n)).toFixed(2);
-  const stringedDecimal = (onlyDecimals + '').split('.');
-  const finalNumber = parseInt(stringedDecimal[1], 10);
-  return finalNumber;
+  const onlyDecimals = (n - Math.floor(n)).toFixed(2); // Extracts decimals
+  const stringedDecimal = (onlyDecimals + '').split('.'); // converts to string and removes first portion
+  const finalAsNumber = parseInt(stringedDecimal[1], 10);
+
+  if (finalAsNumber > 0) {
+    return finalAsNumber;
+  }
+
+  return ''
 }
 
 module.exports = getDecimals;
--- server/helpers/getDecimals.spec.js
+++ server/helpers/getDecimals.spec.js
@@ -4,10 +4,25 @@ describe('Shows only decimals from any Number', () => {
 
   it('gets Only Decimals from a good Number', () => {
     expect(getDecimals(54.46)).toBe(46);
+    expect(getDecimals(54.46890)).toBe(47);
+    expect(getDecimals(54.45990)).toBe(46);
   });
 
   it('gets Only Decimals from a negative Number', () => {
     expect(getDecimals(-54.46)).toBe(46);
+  });
+
+  it('gets Only Decimals from a string', () => {
+    expect(getDecimals('54.46')).toBe(46);
+  });
+
+  it('gets Only Decimals from a string using comma', () => {
+    expect(getDecimals('54,46')).toBe(46);
+  });
+
+  it('works with entire Numbers', () => {
+    expect(getDecimals('54')).toBe('');
+    expect(getDecimals('20004')).toBe('');
   });
 
 });
