33fc03ba6b54e7ce7b43ecf830cc273e701df741
romulo bordezani <romulobordezani@gmail.com>
2019-07-29 21:02:20 -0300

    Minor improvements to the Search Box

--- components/SearchBox/SearchBox.jsx
+++ components/SearchBox/SearchBox.jsx
@@ -4,23 +4,38 @@ import styles from './SearchBox.scss';
 
 const SearchBox = () => {
   const [q, setQ] = useState('');
+  const [animation, setAnimation] = useState('');
+  const placeHolderDefault = 'Nunca dejes de buscar';
+  const [placeHolder, setPlaceHolder] = useState(placeHolderDefault);
 
   const submitQuery = e => {
     e.preventDefault();
-    // TODO - Put some decent error feed back here
+
     if (q !== ``) {
       Router.push(`/items?q=${q}&limit=4`);
+      return null;
     }
+
+    setAnimation(styles['search-box__error']);
+    setPlaceHolder('Y o que buscas hoy?');
+
+    setTimeout(() => {
+      setAnimation('');
+    }, 500);
   };
 
   return (
-    <div className={styles['search-box']} onSubmit={submitQuery}>
+    <div className={`${animation} ${styles['search-box']}`} onSubmit={submitQuery}>
       <form autoComplete="on">
         <input
           className={styles['search-box_field']}
           type="text"
           onChange={event => setQ(event.target.value)}
-          placeholder="Nunca dejes de buscar"
+          onFocus={() => {
+            setPlaceHolder(placeHolderDefault);
+          }}
+          placeholder={placeHolder}
+          autoFocus
         />
         <button className={styles['search-box_submit-button']} type="submit" />
       </form>
